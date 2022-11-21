085d3263cd40b63588cafb02190d8e2f86547711
Nayana Biyani <naina.savadi@gmail.com>
2022-11-22 00:34:44 +0100

    Add XML Test Reports

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ mapfile -t lines < <(echo "$INPUT_TESTS" | grep -v "^$")
 exit_code=0
 for line in "${lines[@]}"; do
   read -r -a args <<< "$line"
-  cmd="/opt/openjdk-13/bin/java $JAVA_OPTS -jar /karate.jar ${args[@]} $INPUT_OPTIONS"
+  cmd="/opt/openjdk-13/bin/java $JAVA_OPTS -jar /karate.jar -o ./results -f junit:xml ${args[@]} $INPUT_OPTIONS"
   echo "- Running: $cmd"
   eval "$cmd" || exit_code=1
 done
